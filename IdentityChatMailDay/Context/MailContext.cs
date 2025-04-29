4c310bdde82be8e03d85541433ff249f2ea85f39
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-29 15:44:29 +0300

    Add project files.

---
+++ IdentityChatMailDay/Context/MailContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityChatMailDay.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatMailDay.Context
+{
+    public class MailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial Catalog=EmailChatDb;integrated Security=true;trust server certificate=true");
+        }
+    }
+}
