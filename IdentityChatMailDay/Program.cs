ba0bd4c73733b37c39d2d934487681baac39dfd0
MuratYucedag <yucedagmurat23@gmail.com>
2025-05-01 16:10:31 +0300

    ...

--- IdentityChatMailDay/Program.cs
+++ IdentityChatMailDay/Program.cs
@@ -1,6 +1,15 @@
+using IdentityChatMailDay.Context;
+using IdentityChatMailDay.Entities;
+using IdentityChatMailDay.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<MailContext>();
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<MailContext>().AddErrorDescriber<CustomIdentityValidator>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
